65e7f205bfab7aaff36cfdee0897aa26908af2e2
Jhan Diaz <jhandi77@gmail.com>
2022-04-08 18:06:29 -0300

    resolucion del problema de la vista

--- src/app.js
+++ src/app.js
@@ -9,16 +9,16 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 /* Enrutadores */
-const indexRouter = require('./routes/indexRouter');
-const productsRouter = require('./routes/productsRouter');
-const usersRouter = require('./routes/usersRouter');
+const indexRouter = require('./routers/indexRouter');
+const productsRouter = require('./routers/productsRouter');
+const userRouter = require('./routers/userRouter');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 /* Middleware de rutas */
 app.use('/', indexRouter); // HOME - Contact 
 app.use('/productos', productsRouter); // Listado, detalle
-app.use('/usuarios', usersRouter); //Login, registro, perfil
+app.use('/usuarios', userRouter); //Login, registro, perfil
 
 
 
--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,11 +3,7 @@ module.exports = {
     /* envia la vista */
     home: (req, res) =>{
         /* reenderiza la vista ejs */
-        res.render('home', {
-            titulo: "Anubisshop",
-            titulo_producto: "Nuevo",
-            descuento: "descuentos",
-        })
+        res.render('home')
     },
     contact: (req, res) => res.send("Contacto")
 }
