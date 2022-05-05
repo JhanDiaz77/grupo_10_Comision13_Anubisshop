d805ffa6ca63d7063a34687e22507013861ca454
Jhan Diaz <jhandi77@gmail.com>
2022-05-05 16:43:21 -0300

    crud de register

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,3 +1,5 @@
+const{getUsers, writeUsers } = require('../data')
+
 module.exports = {
     login: (req,res)  => {
          res.render('users/login')
@@ -5,7 +7,34 @@ module.exports = {
 
     register: (req,res)  => {
          res.render('users/register')
-    }
+    },
+    processRegister: (req,res)  => {
+         let lastId = 0;
+         getUsers.forEach(user => {
+              if(user.id > lastId){
+                   lastId = user.id
+              }
+              
+         });
+
+         let newUser = {
+              id: lastId +1,
+              name: req.body.name,
+              email: req.body.email,
+              password: req.body.password,
+              avatar: req.file ? req.file.filename : ""
+
+         }
+
+         getUsers.push(newUser)
+
+         writeUsers(getUsers)
+
+
+
+              
+}
+
     
 }
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/userController')
+const userController = require('../controller/userController');
+const upload = require('../middlewares/uploadAvatar')
 
 router.get('/login', userController.login);
 router.get('/register', userController.register);
+router.post('/registro', upload.single('avatar'), userController.processRegister)
 
 
 
