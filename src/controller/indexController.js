8be45faab98b1e23ce3ea577d4ace452af620f0c
Sebastian <sebastianchoquemamani@gmail.com>
2022-05-08 23:31:49 -0300

    haciendo funcionar el search

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
+const removeAccents = (str) => {
+	return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  }
 
 const controller = {
     index: (req,res)  => {
@@ -13,6 +15,21 @@ const controller = {
 			productsDestacado,
 			productsOferta
 		})
+    },
+    search: (req,res)  => {
+
+        let searchResult = [];
+
+		products.forEach(product => {
+			if(removeAccents(product.name.toLowerCase()).includes(req.query.keywords.toLowerCase())){
+				searchResult.push(product)
+			}
+		});
+
+		res.render('result', {
+			searchResult,
+			keyword: req.query.keywords,
+		});
     }
     
 }
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const indexController = require('../controller/indexController')
 
 router.get('/', indexController.index);
+router.get('/search', indexController.search);
 
 
 module.exports = router;
